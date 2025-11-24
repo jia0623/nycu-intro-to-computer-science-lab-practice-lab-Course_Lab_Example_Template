f2b4ef85ea01a79d95927acff6e03ec55b68a0ee
jia0623 <ss653511@gmail.com>
2025-11-25 00:17:08 +0800

    Update p2.cpp

--- src/p2.cpp
+++ src/p2.cpp
@@ -1,21 +1,15 @@
-// p2: Recursion Sum (遞迴求和)
 #include <iostream>
 using namespace std;
-
-// TODO: 實作 sum 遞迴函數
-// 參數: n (正整數)
-// 回傳: 1 + 2 + 3 + ... + n 的總和
-// 提示: Base case: n == 1 時回傳 1
-//       Recursive case: 回傳 n + sum(n-1)
-int sum(int n) {
-    // 在此實作你的程式碼
-    return 0;
+int sum(int n){
+  if (n==1)
+    return 1;
+  return sum(n-1)+n;
 }
-
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    // TODO: 讀取 n 並呼叫 sum 函數，輸出結果
-    
+int main()
+{
+  int n;
+  cin>>n;
+  cout<<sum(n)<<endl;
+  
     return 0;
 }
